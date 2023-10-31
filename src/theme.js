175a2691cca305e8adda6a9af8ce80456c3ac457
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-10-31 22:26:46 +0200

    done

---
+++ src/theme.js
@@ -0,0 +1,62 @@
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1B4B66',
+        },
+        secondary: {
+            main: '#F1F1F1',
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '8px',
+                },
+            },
+        },
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '8px',
+                },
+            },
+        },
+        MuiBox: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '8px',
+                },
+            },
+        },
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '8px',
+                },
+            },
+        },
+        MuiTypography: {
+            styleOverrides: {
+                h2: {
+                    '@media (max-width: 600px)': {
+                        fontSize: '1.5rem',
+                    },
+                },
+            },
+        },
+        MuiRating: {
+            styleOverrides: {
+                iconFilled: {
+                    color: '#FF8C4B', 
+                },
+                iconHover: {
+                    color: '#FF8C4B',                                       
+                },
+            },
+        },
+        // Add more components with custom style here
+    },
+});
+export default theme;
