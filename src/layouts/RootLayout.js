72a3ccb34d1ac0d687d173f43873793321ff517a
Farah Alhamad <farah.alhamad9@gmail.com>
2023-11-02 01:45:17 +0200

    Revert "fix - import -"
    This reverts commit b2f70c2c3a1932b837a027c3f49ee12d40f3dd29.

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,42 +1,14 @@
 import React from "react";
-import { Header } from "../components/header/Header";
-import { Footer } from "../components/footer/Footer";
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material";
-
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#1b4b66",
-    },
-    common: {
-      black: "#000000",
-    },
-  },
-  components: {
-    MuiAppBar: {
-      styleOverrides: {
-        colorPrimary: {
-          backgroundColor: "white",
-        },
-      },
-    },
-  },
-  typography: {
-    fontFamily: `"Inter", "Nunito Sans", sans-serif`,
-    fontSize: 14,
-  },
-  text: {
-    primary: "black",
-  },
-});
 
 export const RootLayout = () => {
   return (
-    <ThemeProvider theme={theme}>
+    <div className="root-layout">
       <Header />
       <Outlet />
       <Footer />
-    </ThemeProvider>
+    </div>
   );
 };
