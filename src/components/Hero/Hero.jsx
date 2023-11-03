588422e72059afdcccad5b6e1a4744d9d0303a5e
Farah Alhamad <farah.alhamad9@gmail.com>
2023-11-04 00:08:13 +0200

    merge fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import HeroItem from '../HeroItem/HeroItem';
-import { heroItems } from '../../assets/data/data';
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import HeroItem from "../HeroItem/HeroItem";
+import { heroItems } from "../../assets/data/data";
 export const Hero = () => {
   const carouselSettings = {
     navButtonsProps: {
@@ -22,4 +22,4 @@ export const Hero = () => {
       ))}
     </Carousel>
   );
-}
+};
