abdafb76bc3fcaf7ba08b4b8b3a8bd2ee3dc541d
Farah Alhamad <farah.alhamad9@gmail.com>
2023-11-08 21:11:00 +0200

    product-galary-added

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,12 +1,23 @@
-import React , {useState} from "react";
-import Breadcrumb ,{StyledLink} from "../../components/Breadcrumbs/Breadcrumbs";
-import { useParams } from 'react-router-dom';
-import { useDataActions } from '../../hooks/useDataActions';
-import { Container , Typography , Grid , Box , Skeleton , Tabs ,Tab} from "@mui/material";
+import React, { useState } from "react";
+import Breadcrumb, {
+  StyledLink,
+} from "../../components/Breadcrumbs/Breadcrumbs";
+import { useParams } from "react-router-dom";
+import { useDataActions } from "../../hooks/useDataActions";
+import {
+  Container,
+  Typography,
+  Grid,
+  Box,
+  Skeleton,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import { ProductInfo } from "../../components/ProductInfo/ProductInfo";
 import { ProductDescription } from "./partials/ProductDescription";
 import { ProductReviews } from "./partials/ProductReviews";
 import { RelatedProducts } from "./partials/RelatedProducts";
+import { ProductImagesGalary } from "../../components/ProductImagesGalary/ProductImagesGalary";
 
 export const ProductPage = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -17,28 +28,27 @@ export const ProductPage = () => {
   };
   const TabsItems = [
     {
-      'label': 'Product Description',
-      'content': <ProductDescription productId={id} />
+      label: "Product Description",
+      content: <ProductDescription productId={id} />,
     },
     {
-      'label': 'Related Products',
-      'content': <RelatedProducts productId={id} />
+      label: "Related Products",
+      content: <RelatedProducts productId={id} />,
     },
     {
-      'label': 'Ratings and Reviews',
-      'content': <ProductReviews productId={id} />
-    }
-  ]
-  console.log('Product ID:', id);
+      label: "Ratings and Reviews",
+      content: <ProductReviews productId={id} />,
+    },
+  ];
+  console.log("Product ID:", id);
   const { useProductDetails } = useDataActions();
   const { data: product, isLoading, isError } = useProductDetails(id);
+  console.log("data", product);
   const breadcrumbItems = [
-    <StyledLink key="2">
-      Handbags
-    </StyledLink>,
+    <StyledLink key="2">Handbags</StyledLink>,
     <Typography underline="hover" key="3">
       Label
-    </Typography>
+    </Typography>,
   ];
   if (isError) return <p>Error ...</p>;
   return (
@@ -48,31 +58,47 @@ export const ProductPage = () => {
       </Box>
       <Grid container spacing={1} mt={0.5}>
         <Grid item xs={12} sm={6} md={4} lg={6}>
-          {isLoading ? (<Skeleton variant="rectangular" width={'100%'} height={412} />) :
-            (<>{/* farah you can add your carousel here */}</>)}
+          {isLoading ? (
+            <Skeleton variant="rectangular" width={"100%"} height={412} />
+          ) : (
+            <ProductImagesGalary images={product.product.image_url} />
+          )}
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={6}>
-          {
-            isLoading ? (Array.from({ length: 6 }).map((_, index) => (
+          {isLoading ? (
+            Array.from({ length: 6 }).map((_, index) => (
               <Box key={index} mb={4}>
-                <Skeleton animation="wave" width={'100%'} height={'40px'} /></Box>)))
-              : (<ProductInfo {...product} />)
-          }
+                <Skeleton animation="wave" width={"100%"} height={"40px"} />
+              </Box>
+            ))
+          ) : (
+            <ProductInfo {...product} />
+          )}
         </Grid>
       </Grid>
-      {isLoading ? (<></>)
-        : (<Box my={3}>
-          <Tabs value={selectedTab} onChange={handleTabChange} TabIndicatorProps={{ style: { display: "none" } }}>
+      {isLoading ? (
+        <></>
+      ) : (
+        <Box my={3}>
+          <Tabs
+            value={selectedTab}
+            onChange={handleTabChange}
+            TabIndicatorProps={{ style: { display: "none" } }}
+          >
             {TabsItems.map((tab, index) => (
-              <Tab label={tab.label} key={index} />))}
+              <Tab label={tab.label} key={index} />
+            ))}
           </Tabs>
-          {TabsItems.map((tab, index) => (
-            selectedTab === index && (
-              <Box key={index} mt={3}>
-                {tab.content}
-              </Box>)))}
-        </Box>)
-      }
+          {TabsItems.map(
+            (tab, index) =>
+              selectedTab === index && (
+                <Box key={index} mt={3}>
+                  {tab.content}
+                </Box>
+              )
+          )}
+        </Box>
+      )}
     </Container>
   );
-}
+};
