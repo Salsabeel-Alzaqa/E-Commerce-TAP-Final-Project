64bd5823f06f8ca9779c09913bd1af41f2501959
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-11-18 14:48:00 +0200

    add LogInGuard

--- src/pages/AuthGuard/AuthGuard.jsx
+++ src/pages/AuthGuard/AuthGuard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-export const AuthGuard = ({ children }) => {
+const AuthGuard = ({ children }) => {
   const isAuthenticated = localStorage.getItem('token');
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace={true}/>;
   }
   return <>{children}</>;
-};
+}; 
+
+export function LoginGuard({ children }) {
+  const isAuthenticated = localStorage.getItem("token");
+  if (isAuthenticated) return <Navigate to="/" replace={true}/>;
+  return <>{children}</>;
+}
+
+export default AuthGuard;
