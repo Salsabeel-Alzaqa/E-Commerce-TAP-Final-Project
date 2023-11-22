696f5e0d92a980e9f57078ebca0e67bb3a701c63
Farah Alhamad <farah.alhamad9@gmail.com>
2023-11-23 00:10:56 +0200

    merge fix

--- src/components/ProductsCart/ProductsCart.jsx
+++ src/components/ProductsCart/ProductsCart.jsx
@@ -9,17 +9,10 @@ import Paper from "@mui/material/Paper";
 import { ProductCartCard } from "../ProductCartCard/ProductCartCard";
 import { Button, useTheme } from "@mui/material";
 import { useDataActions } from "../../hooks/useDataActions";
-
+import { useWishlist } from "../../hooks/useWishlist";
+import { ConfirmationModal } from "../ConfirmationModal/ConfirmationModal";
 export const ProductsCart = (props) => {
   const theme = useTheme();
-  const { useRemoveCartItem } = useDataActions();
-  const { isLoading, isError, error, mutateAsync } = useRemoveCartItem();
-
-  const handleRemoveItem = async (itemId) => {
-    await mutateAsync(itemId);
-    props.setCartItems(props.cartItems.filter((item) => item.id !== itemId));
-  };
-
   return (
     <TableContainer component={Paper} variant="none">
       <Table aria-label="simple table">
@@ -73,123 +66,171 @@ export const ProductsCart = (props) => {
         <TableBody>
           {props.cartItems.map((item, index) => (
             <React.Fragment key={index}>
-              <TableRow
-                sx={{
-                  "&:last-child td, &:last-child th": { border: 0 },
-                }}
-              >
-                <TableCell
-                  component="th"
-                  scope="row"
-                  sx={{
-                    border: "none",
-                    [theme.breakpoints.down("sm")]: {
-                      paddingRight: "0",
-                    },
-                  }}
-                >
-                  <ProductCartCard
-                    setCartItems={props.setCartItems}
-                    item={item}
-                    inMyCart={true}
-                    orderpage={props.orderpage}
-                  />
-                </TableCell>
-                <TableCell
-                  align="right"
-                  sx={{
-                    border: "none",
-                    paddingBottom: 0,
-                    [theme.breakpoints.down("sm")]: {
-                      fontSize: "12px",
-                    },
-                  }}
-                >
-                  ${item.sub_total}
-                </TableCell>
-                <TableCell
-                  align="right"
-                  sx={{
-                    border: "none",
-                    paddingBottom: 0,
-                    [theme.breakpoints.down("sm")]: {
-                      fontSize: "12px",
-                    },
-                  }}
-                >
-                  {item.quantity}
-                </TableCell>
-                <TableCell
-                  align="right"
-                  sx={{
-                    border: "none",
-                    paddingBottom: 0,
-                    [theme.breakpoints.down("sm")]: {
-                      fontSize: "12px",
-                    },
-                  }}
-                >
-                  ${item.sub_total * item.quantity}
-                </TableCell>
-              </TableRow>
-              {props.orderpage ? null
-                :<TableRow>
-                  <TableCell
-                    colSpan={theme.breakpoints.down("sm") ? 2 : 3}
-                    align="right"
-                    sx={{
-                      paddingTop: 0,
-                      borderBottom: "none",
-                      [theme.breakpoints.down("sm")]: {
-                        fontSize: "12px",
-                        paddingLeft: "0",
-                      },
-                    }}
-                  >
-                    <Button
-                      sx={{
-                        textTransform: "none",
-                        textDecoration: "underline",
-                        color: "#1B4B66",
-                      }}
-                      onClick={() => handleRemoveItem(item.id)}
-                    >
-                      Move to Wishlist
-                    </Button>
-                  </TableCell>
-                  <TableCell
-                    colSpan={3}
-                    align="right"
-                    sx={{
-                      paddingTop: 0,
-                      borderBottom: "none",
-                      [theme.breakpoints.down("sm")]: {
-                        fontSize: "12px",
-                        paddingLeft: "0",
-                      },
-                    }}
-                  >
-                    <Button
-                      aria-label="remove"
-                      color="error"
-                      sx={{
-                        textTransform: "none",
-                        textDecoration: "underline",
-                        [theme.breakpoints.down("sm")]: {
-                          fontSize: "12px",
-                        },
-                      }}
-                      onClick={() => handleRemoveItem(item.id)}
-                    >
-                      Remove
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              }
+              <CartItem
+                item={item}
+                orderpage={props.orderpage}
+                setCartItems={props.setCartItems}
+                cartItems={props.cartItems}
+              />
             </React.Fragment>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
-  )
+  );
+};
+const CartItem = ({ item, orderpage, setCartItems, cartItems }) => {
+  const [isConfirmationModalOpen, setConfirmationModalOpen] =
+    React.useState(false);
+  const theme = useTheme();
+  const { useRemoveCartItem } = useDataActions();
+  const { mutateAsync } = useRemoveCartItem();
+  const {
+    isInWishlist,
+    AddToWishlist,
+    isAddWishlistProductLoading,
+    isWishlistLoading,
+  } = useWishlist(item.productID);
+  const handleRemoveItem = async () => {
+    await mutateAsync(item.id);
+    setCartItems(cartItems.filter((product) => product.id !== item.id));
+  };
+  const handleMoveToWishlist = () => {
+    handleRemoveItem();
+    AddToWishlist();
+  };
+  const openConfirmationModal = () => {
+    setConfirmationModalOpen(true);
+  };
+  const closeConfirmationModal = () => {
+    setConfirmationModalOpen(false);
+  };
+  return (
+    <>
+      <TableRow
+        sx={{
+          "&:last-child td, &:last-child th": { border: 0 },
+        }}
+      >
+        <TableCell
+          component="th"
+          scope="row"
+          sx={{
+            border: "none",
+            [theme.breakpoints.down("sm")]: {
+              paddingRight: "0",
+            },
+          }}
+        >
+          <ProductCartCard
+            setCartItems={setCartItems}
+            item={item}
+            inMyCart={true}
+            orderpage={orderpage}
+          />
+        </TableCell>
+        <TableCell
+          align="right"
+          sx={{
+            border: "none",
+            paddingBottom: 0,
+            [theme.breakpoints.down("sm")]: {
+              fontSize: "12px",
+            },
+          }}
+        >
+          ${item.sub_total}
+        </TableCell>
+        <TableCell
+          align="right"
+          sx={{
+            border: "none",
+            paddingBottom: 0,
+            [theme.breakpoints.down("sm")]: {
+              fontSize: "12px",
+            },
+          }}
+        >
+          {item.quantity}
+        </TableCell>
+        <TableCell
+          align="right"
+          sx={{
+            border: "none",
+            paddingBottom: 0,
+            [theme.breakpoints.down("sm")]: {
+              fontSize: "12px",
+            },
+          }}
+        >
+          ${item.sub_total * item.quantity}
+        </TableCell>
+      </TableRow>
+      {orderpage ? null : (
+        <TableRow>
+          <TableCell
+            colSpan={theme.breakpoints.down("sm") ? 2 : 3}
+            align="right"
+            sx={{
+              paddingTop: 0,
+              borderBottom: "none",
+              [theme.breakpoints.down("sm")]: {
+                fontSize: "12px",
+                paddingLeft: "0",
+              },
+            }}
+          >
+            {isInWishlist || isWishlistLoading ? (
+              <></>
+            ) : (
+              <Button
+                sx={{
+                  textTransform: "none",
+                  textDecoration: "underline",
+                  color: "#1B4B66",
+                }}
+                disabled={isAddWishlistProductLoading}
+                onClick={handleMoveToWishlist}
+              >
+                Move to Wishlist
+              </Button>
+            )}
+          </TableCell>
+          <TableCell
+            colSpan={3}
+            align="right"
+            sx={{
+              paddingTop: 0,
+              borderBottom: "none",
+              [theme.breakpoints.down("sm")]: {
+                fontSize: "12px",
+                paddingLeft: "0",
+              },
+            }}
+          >
+            <Button
+              aria-label="remove"
+              color="error"
+              sx={{
+                textTransform: "none",
+                textDecoration: "underline",
+                [theme.breakpoints.down("sm")]: {
+                  fontSize: "12px",
+                },
+              }}
+              onClick={openConfirmationModal}
+            >
+              Remove
+            </Button>
+          </TableCell>
+        </TableRow>
+      )}
+      <ConfirmationModal
+        isOpen={isConfirmationModalOpen}
+        onClose={closeConfirmationModal}
+        onConfirm={handleRemoveItem}
+        message={`Are you sure that you want to remove '${item.name}' from your cart?`}
+      />
+    </>
+  );
 };
